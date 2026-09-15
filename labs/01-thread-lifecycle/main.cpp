52ab1461d6c3325c20cd5dec92107d010d52fc69
Edward NoaLand <edwardnoaland@gmail.com>
2026-09-15 14:42:12 +0000

    feat(thread): use vector to manage jthread

--- labs/01-thread-lifecycle/main.cpp
+++ labs/01-thread-lifecycle/main.cpp
@@ -2,18 +2,29 @@
 #include <print>
 #include <string>
 #include <thread>
+#include <array>
+#include <vector>
+
 
 void greeting(std::string_view name) {
     std::println("hello from {}", name);
 }
 
 int main() {
-    std::latch work_done{1};
+    std::array<std::string, 3> worker_names{"a", "b", "c"};
+    std::vector<std::jthread> thread_pool;
 
-    std::jthread worker = std::jthread([&work_done](){
-        greeting("worker");
-        work_done.count_down();
-    });
+    std::latch work_done{worker_names.size()};
+
+    for (const auto& name : worker_names) {
+        thread_pool.emplace_back(std::jthread{[&work_done, name](){
+            using namespace std::chrono_literals;
+            greeting("worker-" + name);
+            work_done.count_down();
+            std::this_thread::sleep_for(3s);
+            std::println("do more work from worker-{}", name);
+        }});
+    }
 
     // Make sure that the code will print worker first and then main
     // solution 1: use join

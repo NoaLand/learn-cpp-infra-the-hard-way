f9b368327daff15ef05a30dcf921f6be908d46b0
Edward NoaLand <edwardnoaland@gmail.com>
2026-09-15 14:24:01 +0000

    feat(thread): use latch instead of join

--- labs/01-thread-lifecycle/main.cpp
+++ labs/01-thread-lifecycle/main.cpp
@@ -1,3 +1,4 @@
+#include <latch>
 #include <print>
 #include <string>
 #include <thread>
@@ -7,13 +8,19 @@ void greeting(std::string_view name) {
 }
 
 int main() {
-    std::jthread worker = std::jthread([](){
+    std::latch work_done{1};
+
+    std::jthread worker = std::jthread([&work_done](){
         greeting("worker");
+        work_done.count_down();
     });
 
     // Make sure that the code will print worker first and then main
-    // solution 1
-    worker.join();
+    // solution 1: use join
+    // worker.join();
+
+    // solution 2: use latch
+    work_done.wait();
 
     greeting("main");
 

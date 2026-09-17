217642955364a280c98971ab6e836975a92556f1
Edward NoaLand <edwardnoaland@gmail.com>
2026-09-17 15:26:34 +0000

    refator(shared counter): should use join instead of latch to make code semantically

--- labs/01-thread-lifecycle/shared_counter.cpp
+++ labs/01-thread-lifecycle/shared_counter.cpp
@@ -3,19 +3,16 @@
 #include <string>
 #include <thread>
 #include <vector>
-#include <latch>
 
 int main() {
     int counter{};
 
     std::array<std::string, 4> worker_names{"alpha", "beta", "charlie", "delta"};
-    std::latch waiting_all_threads_done{worker_names.size()};
-    auto worker = [&counter, &waiting_all_threads_done](std::string name){
+    auto worker = [&counter](std::string name){
         std::println("{} start working", name);
         for (int i = 0; i < 100000; ++i) {
             ++counter;
         }
-        waiting_all_threads_done.count_down();
         std::println("{} stop working", name);
     };
 
@@ -25,7 +22,10 @@ int main() {
         thread_pool.emplace_back(worker, name);
     }
 
-    waiting_all_threads_done.wait();
+    for (auto& thread : thread_pool) {
+        thread.join();
+    }
+
     std::println("in the end the counter is: {}", counter);
 
     return 0;

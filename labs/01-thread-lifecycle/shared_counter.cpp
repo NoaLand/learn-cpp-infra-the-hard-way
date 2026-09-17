04ead001971e99c8bd7a9332e5086b5addcce9ec
Edward NoaLand <edwardnoaland@gmail.com>
2026-09-17 15:25:06 +0000

    feat(shared counter): introduce data race in purpose

--- labs/01-thread-lifecycle/shared_counter.cpp
+++ labs/01-thread-lifecycle/shared_counter.cpp
@@ -1,7 +1,32 @@
 #include <print>
+#include <array>
+#include <string>
+#include <thread>
+#include <vector>
+#include <latch>
 
 int main() {
-    std::println("hello world!");
+    int counter{};
+
+    std::array<std::string, 4> worker_names{"alpha", "beta", "charlie", "delta"};
+    std::latch waiting_all_threads_done{worker_names.size()};
+    auto worker = [&counter, &waiting_all_threads_done](std::string name){
+        std::println("{} start working", name);
+        for (int i = 0; i < 100000; ++i) {
+            ++counter;
+        }
+        waiting_all_threads_done.count_down();
+        std::println("{} stop working", name);
+    };
+
+    std::vector<std::jthread> thread_pool;
+    thread_pool.reserve(worker_names.size());
+    for (const auto& name : worker_names) {
+        thread_pool.emplace_back(worker, name);
+    }
+
+    waiting_all_threads_done.wait();
+    std::println("in the end the counter is: {}", counter);
 
     return 0;
 }

14d06bb9def4703c1570ef63851bb35c6a3491a8
Edward NoaLand <edwardnoaland@gmail.com>
2026-09-18 12:29:47 +0000

    refactor(shared counter): remove unused include

--- labs/01_thread_lifecycle/shared_counter/include/labs/threads/shared_counter/local_reduce.h
+++ labs/01_thread_lifecycle/shared_counter/include/labs/threads/shared_counter/local_reduce.h
@@ -2,8 +2,6 @@
 
 #include "labs/threads/shared_counter/shared_counter.h"
 
-#include <vector>
-
 namespace cpp_infra_labs::thread::shared_counter {
     struct local_reduce : public shared_counter {
         void run() override;

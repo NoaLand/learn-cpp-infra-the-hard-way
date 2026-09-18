24b8e0b65c1d16f6043ac9fdaadfbf63aef01cd3
Edward NoaLand <edwardnoaland@gmail.com>
2026-09-18 12:55:41 +0000

    refactor(shared counter): extract worker as an isolated function

--- labs/01_thread_lifecycle/shared_counter/src/atomic_shared_int.cpp
+++ labs/01_thread_lifecycle/shared_counter/src/atomic_shared_int.cpp
@@ -4,20 +4,20 @@
 
 namespace cpp_infra_labs::thread::shared_counter {
     void atomic_shared_int::run() {
-        auto worker = [&sum = counter](std::string name){
-            for (int i = 0; i < 100000; ++i) {
-                sum.fetch_add(1);
-            }
-        };
-
         thread_pool.reserve(worker_names.size());
 
         for (const auto& name : worker_names) {
-            thread_pool.emplace_back(worker, name);
+            thread_pool.emplace_back(&atomic_shared_int::worker, this, name);
         }
 
         for (auto& thread : thread_pool) {
             thread.join();
+        }
+    }
+
+    void atomic_shared_int::worker(std::string name) {
+        for (int i = 0; i < 100000; ++i) {
+            counter.fetch_add(1);
         }
     }
 }

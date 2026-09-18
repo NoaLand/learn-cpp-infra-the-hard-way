c6eff150be4dcfb267656c841f4139754c647d24
Edward NoaLand <edwardnoaland@gmail.com>
2026-09-18 13:05:28 +0000

    refactor(shared counter): extract worker as an isolated function in local reduce

--- labs/01_thread_lifecycle/shared_counter/include/labs/threads/shared_counter/local_reduce.h
+++ labs/01_thread_lifecycle/shared_counter/include/labs/threads/shared_counter/local_reduce.h
@@ -2,10 +2,14 @@
 
 #include "labs/threads/shared_counter/shared_counter.h"
 
+#include <vector>
+
 namespace cpp_infra_labs::thread::shared_counter {
     struct local_reduce : public shared_counter {
         void run() override;
 
         int counter{};
+    private:
+        void worker(std::string name, std::vector<int>& counter_for_each_thread, int index);
     };
 }
--- labs/01_thread_lifecycle/shared_counter/src/local_reduce.cpp
+++ labs/01_thread_lifecycle/shared_counter/src/local_reduce.cpp
@@ -5,19 +5,11 @@
 
 namespace cpp_infra_labs::thread::shared_counter {
     void local_reduce::run() {
-        std::vector<int> counter_for_each_thread(worker_names.size());
-        auto worker = [&counter_for_each_thread](std::string name, std::size_t index){
-            int local_sum{};
-            for (int i = 0; i < 100000; ++i) {
-                ++local_sum;
-            }
-            counter_for_each_thread[index] = local_sum;
-        };
-
         thread_pool.reserve(worker_names.size());
 
+        std::vector<int> counter_for_each_thread(worker_names.size());
         for (std::size_t i = 0; i < worker_names.size(); ++i) {
-            thread_pool.emplace_back(worker, worker_names[i], i);
+            thread_pool.emplace_back(&local_reduce::worker, this, worker_names[i], std::ref(counter_for_each_thread), i);
         }
 
         for (auto& thread : thread_pool) {
@@ -25,5 +17,13 @@ namespace cpp_infra_labs::thread::shared_counter {
         }
 
         counter = std::accumulate(counter_for_each_thread.begin(), counter_for_each_thread.end(), 0);
+    }
+
+    void local_reduce::worker(std::string name, std::vector<int>& counter_for_each_thread, int index) {
+        int local_sum{};
+        for (int i = 0; i < 100000; ++i) {
+            ++local_sum;
+        }
+        counter_for_each_thread[index] = local_sum;
     }
 }

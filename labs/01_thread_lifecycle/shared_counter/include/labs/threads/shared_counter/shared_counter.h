994dc372a42a973063aa5f6acccc3e067f14e153
Edward NoaLand <edwardnoaland@gmail.com>
2026-09-18 15:33:25 +0000

    feat(shared counter): run self increment 10000000 times

--- labs/01_thread_lifecycle/shared_counter/include/labs/threads/shared_counter/shared_counter.h
+++ labs/01_thread_lifecycle/shared_counter/include/labs/threads/shared_counter/shared_counter.h
@@ -14,7 +14,7 @@ namespace cpp_infra_labs::thread::shared_counter {
 
         std::array<std::string, 4> worker_names{"alpha", "beta", "charlie", "delta"};
         std::vector<std::thread> thread_pool;
-        std::size_t run_times{100000};
+        std::size_t run_times{10000000};
         std::size_t expected{run_times * worker_names.size()};
     };
 }

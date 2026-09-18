8c28ed6e6e471da7fd695cbe73f0ea3d68a0fde9
Edward NoaLand <edwardnoaland@gmail.com>
2026-09-18 12:05:42 +0000

    feat(shared counter): add wall clock to calculate performance

--- labs/01_thread_lifecycle/shared_counter/src/shared_counter.cpp
+++ labs/01_thread_lifecycle/shared_counter/src/shared_counter.cpp
@@ -1,3 +1,4 @@
+#include <chrono>
 #include <print>
 
 #include "labs/threads/shared_counter/unsafe_shared_int.h"
@@ -8,24 +9,37 @@
 
 int main() {
     std::println("Start unsafe shared int\n");
+    auto unsafe_shared_int_start_ = std::chrono::high_resolution_clock::now();
     auto unsafe_shared_int_ = cpp_infra_labs::thread::shared_counter::unsafe_shared_int{};
     unsafe_shared_int_.run();
+    auto unsafe_shared_int_end_ = std::chrono::high_resolution_clock::now();
     std::println("\nStop unsafe shared int\n");
 
     std::println("Start mutex per increment\n");
+    auto mutex_per_increment_start_ = std::chrono::high_resolution_clock::now();
     auto mutex_per_increment_ = cpp_infra_labs::thread::shared_counter::mutex_per_increment{};
     mutex_per_increment_.run();
+    auto mutex_per_increment_end_ = std::chrono::high_resolution_clock::now();
     std::println("\nStop mutex per increment\n");
 
     std::println("Start atomic shared int\n");
+    auto atomic_shared_int_start_ = std::chrono::high_resolution_clock::now();
     auto atomic_shared_int_ = cpp_infra_labs::thread::shared_counter::atomic_shared_int{};
     atomic_shared_int_.run();
+    auto atomic_shared_int_end_ = std::chrono::high_resolution_clock::now();
     std::println("\nStop atomic shared int\n");
 
     std::println("Start local reduce\n");
+    auto local_reduce_start_ = std::chrono::high_resolution_clock::now();
     auto local_reduce_ = cpp_infra_labs::thread::shared_counter::local_reduce{};
     local_reduce_.run();
+    auto local_reduce_end_ = std::chrono::high_resolution_clock::now();
     std::println("\nStop local reduce\n");
+
+    std::println("Unsafe shared int duration {}", std::chrono::duration<double, std::milli>(unsafe_shared_int_end_ - unsafe_shared_int_start_));
+    std::println("Mutex per increment duration {}", std::chrono::duration<double, std::milli>(mutex_per_increment_end_ - mutex_per_increment_start_));
+    std::println("Atomic shared int duration {}", std::chrono::duration<double, std::milli>(atomic_shared_int_end_ - atomic_shared_int_start_));
+    std::println("Local reduce duration {}", std::chrono::duration<double, std::milli>(local_reduce_end_ - local_reduce_start_));
 
     return 0;
 }

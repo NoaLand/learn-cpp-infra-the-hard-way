798ca4cd3aaff791626d336fe245f2cd52e759e8
Edward NoaLand <edwardnoaland@gmail.com>
2026-09-18 16:49:58 +0000

    feat(shared counter): support passing param from command line to run perf in the future

--- labs/01_thread_lifecycle/shared_counter/src/shared_counter.cpp
+++ labs/01_thread_lifecycle/shared_counter/src/shared_counter.cpp
@@ -5,6 +5,7 @@
 #include <functional>
 #include <numeric>
 #include <print>
+#include <stdexcept>
 
 #include "labs/threads/shared_counter/unsafe_shared_int.h"
 #include "labs/threads/shared_counter/mutex_per_increment.h"
@@ -45,22 +46,47 @@ void benchmark(std::string test_name, const std::function<void(const T&)>& asser
     std::println("{}'s median running time is: {}\n\n", test_name, (durations.at(4) + durations.at(5)) / 2);
 }
 
-int main() {
-    benchmark<cpp_infra_labs::thread::shared_counter::unsafe_shared_int>("[unsafe shared int]", [](const auto& instance){
-        std::println("data race with counter: {}", instance.counter);
-    });
+int main(int argc, char* argv[]) {
+    if (argc != 2) {
+        throw std::runtime_error{R"(Must pass at least one of the following param: "all", "unsafe", "mutex", "atomic", "local_reduce")"};
+    }
 
-    benchmark<cpp_infra_labs::thread::shared_counter::mutex_per_increment>("[mutex per increment]", [](const auto& instance){
-        assert(instance.counter == instance.expected);
-    });
+    std::string_view param{argv[1]};
+    if (param == "unsafe") {
+        benchmark<cpp_infra_labs::thread::shared_counter::unsafe_shared_int>("[unsafe shared int]", [](const auto& instance){
+            std::println("data race with counter: {}", instance.counter);
+        });
+    } else if (param == "mutex") {
+        benchmark<cpp_infra_labs::thread::shared_counter::mutex_per_increment>("[mutex per increment]", [](const auto& instance){
+            assert(instance.counter == instance.expected);
+        });
+    } else if (param == "atomic") {
+        benchmark<cpp_infra_labs::thread::shared_counter::atomic_shared_int>("[atomic shared int]", [](const auto& instance){
+            assert(instance.counter.load() == instance.expected);
+        });
+    } else if (param == "local_reduce") {
+        benchmark<cpp_infra_labs::thread::shared_counter::local_reduce>("[local reduce]", [](const auto& instance){
+            assert(instance.counter == instance.expected);
+        });
+    } else if (param == "all") {
+        benchmark<cpp_infra_labs::thread::shared_counter::unsafe_shared_int>("[unsafe shared int]", [](const auto& instance){
+            std::println("data race with counter: {}", instance.counter);
+        });
 
-    benchmark<cpp_infra_labs::thread::shared_counter::atomic_shared_int>("[atomic shared int]", [](const auto& instance){
-        assert(instance.counter.load() == instance.expected);
-    });
+        benchmark<cpp_infra_labs::thread::shared_counter::mutex_per_increment>("[mutex per increment]", [](const auto& instance){
+            assert(instance.counter == instance.expected);
+        });
 
-    benchmark<cpp_infra_labs::thread::shared_counter::local_reduce>("[local reduce]", [](const auto& instance){
-        assert(instance.counter == instance.expected);
-    });
+        benchmark<cpp_infra_labs::thread::shared_counter::atomic_shared_int>("[atomic shared int]", [](const auto& instance){
+            assert(instance.counter.load() == instance.expected);
+        });
+
+        benchmark<cpp_infra_labs::thread::shared_counter::local_reduce>("[local reduce]", [](const auto& instance){
+            assert(instance.counter == instance.expected);
+        });
+    } else {
+        throw std::runtime_error{R"(Unknown param. Must be one of: "all", "unsafe", "mutex", "atomic", "local_reduce")"};
+    }
 
     return 0;
 }

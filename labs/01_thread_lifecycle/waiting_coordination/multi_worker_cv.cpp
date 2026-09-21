d231b169e28504d94ad00c24e5ff1ec4c82f3735
Edward NoaLand <edwardnoaland@gmail.com>
2026-09-21 18:01:57 +0000

    feat(waiting coordination): use one cv for both monitor and consumer

--- labs/01_thread_lifecycle/waiting_coordination/multi_worker_cv.cpp
+++ labs/01_thread_lifecycle/waiting_coordination/multi_worker_cv.cpp
@@ -8,6 +8,7 @@
 int main() {
     std::queue<int> task_queue;
     bool is_producer_done{};
+    std::size_t in_flight{};
     std::mutex task_queue_mutex_;
     std::condition_variable cv;
 
@@ -30,7 +31,7 @@ int main() {
         cv.notify_all();
     };
 
-    auto consumer = [&task_queue_mutex_, &cv, &is_producer_done, &task_queue](std::string name){
+    auto consumer = [&task_queue_mutex_, &cv, &is_producer_done, &task_queue, &in_flight](std::string name){
         std::size_t working_time{};
         while (true) {
             int task{};
@@ -47,17 +48,48 @@ int main() {
 
                 task = task_queue.front();
                 task_queue.pop();
+                ++in_flight;
                 ++working_time;
+                cv.notify_one();
             }
             using namespace std::chrono_literals;
-            std::this_thread::sleep_for(15ms);
+            std::this_thread::sleep_for(30ms);
             std::println("{} is consuming: {}", name, task);
+            {
+                std::lock_guard<std::mutex> lock_guard{task_queue_mutex_};
+                --in_flight;
+                cv.notify_one();
+            }
+        }
+    };
+
+    auto monitor = [&task_queue_mutex_, &cv, &is_producer_done, &task_queue, &in_flight](){
+        int old{};
+
+        while (true) {
+            {
+                std::unique_lock<std::mutex> lock{task_queue_mutex_};
+                cv.wait(lock, [&is_producer_done, &in_flight, &old,&task_queue]{
+                    return is_producer_done || in_flight != old || !task_queue.empty();
+                });
+
+                if (is_producer_done && task_queue.empty() && in_flight == 0) {
+                    std::println("[monitor] all work done");
+                    return;
+                }
+
+                if (old != in_flight) {
+                    std::println("[monitor] current working {} tasks", in_flight);
+                    old = in_flight;
+                }
+            }
         }
     };
 
     std::vector<std::jthread> thread_pool;
-    thread_pool.reserve(5);
+    thread_pool.reserve(6);
     thread_pool.emplace_back(producer);
+    thread_pool.emplace_back(monitor);
     thread_pool.emplace_back(consumer, "alpha");
     thread_pool.emplace_back(consumer, "beta");
     thread_pool.emplace_back(consumer, "charlie");

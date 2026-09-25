dceb251e4cb2e729917cf12f9b822bb7e0228a2a
Edward NoaLand <edwardnoaland@gmail.com>
2026-09-25 13:39:00 +0000

    feat(semaphore): implement cv version

--- labs/01_thread_lifecycle/semaphore_and_bounded_concurrency/cv.cpp
+++ labs/01_thread_lifecycle/semaphore_and_bounded_concurrency/cv.cpp
@@ -1,6 +1,59 @@
+#include <condition_variable>
+#include <mutex>
 #include <print>
+#include <string>
+#include <thread>
+#include <array>
+#include <vector>
 
 int main() {
-    std::println("hello world from {}", "semaphore and concurrency cv version");
+    std::mutex worker_mutex_;
+    std::condition_variable cv;
+    int in_flight{};
+
+    std::array<std::string, 8> worker_names{
+        "alpha",
+        "beta",
+        "charlie",
+        "delta",
+        "eva",
+        "frank",
+        "george",
+        "henry"
+    };
+
+    auto worker = [&worker_mutex_, &cv, &in_flight](std::string name){
+        {
+            std::unique_lock<std::mutex> lock{worker_mutex_};
+            cv.wait(lock, [&in_flight]{
+                return in_flight < 3;
+            });
+
+            ++in_flight;
+            std::println("{} entered, in flight={}", name, in_flight);
+        }
+
+        using namespace std::chrono_literals;
+        std::this_thread::sleep_for(1s);
+
+        {
+            std::lock_guard<std::mutex> lock_guard{worker_mutex_};
+            --in_flight;
+            std::println("{} leaving, in flight={}", name, in_flight);
+        }
+
+        cv.notify_one();
+    };
+
+    std::vector<std::jthread> thread_pool;
+    thread_pool.reserve(worker_names.size());
+    for (const auto& name : worker_names) {
+        thread_pool.emplace_back(worker, name);
+    }
+
+    for (auto& worker : thread_pool) {
+        worker.join();
+    }
+
     return 0;
 }
